e30e6d5cfd30e8eaf4d0030f24659db73ea5161f
MichaelDaum <MichaelDaum@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2013-11-06 11:04:36 +0000

    Item8402: cleaning up leftovers'n cruft
    git-svn-id: http://svn.foswiki.org/trunk/BlogPlugin@17049 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Plugins/BlogPlugin.pm
+++ lib/Foswiki/Plugins/BlogPlugin.pm
@@ -1,6 +1,6 @@
 # Plugin for Foswiki - The Free and Open Source Wiki, http://foswiki.org/
 #
-# Copyright (C) 2005-2012 Michael Daum http://michaeldaumconsulting.com
+# Copyright (C) 2005-2013 Michael Daum http://michaeldaumconsulting.com
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -24,7 +24,7 @@ our $core;
 our $VERSION = '2.00';
 our $RELEASE = '2.00';
 our $NO_PREFS_IN_TOPIC = 1;
-our $SHORTDESCRIPTION = 'Basic blogging features used to implement the <nop>BlogApp <nop>WikiApplication';
+our $SHORTDESCRIPTION = 'A blogging system for Foswiki';
 our $baseTopic;
 our $baseWeb;
 
